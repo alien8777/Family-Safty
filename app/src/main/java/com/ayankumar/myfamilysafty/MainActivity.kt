2bc647de363c908b31243058114e7c26087ce93c
DESKTOP-SSEH73Q\Ayan Kumar Jana <bit.ayan8777@gmail.com>
2023-03-18 14:43:41 +0530

    First commit

---
+++ app/src/main/java/com/ayankumar/myfamilysafty/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.ayankumar.myfamilysafty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import com.ayankumar.myfamilysafty.Fragments.DashBoardFragment
+import com.ayankumar.myfamilysafty.Fragments.GuardFragment
+import com.ayankumar.myfamilysafty.Fragments.HomeFragment
+import com.ayankumar.myfamilysafty.Fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.btmnavigationView)
+
+        bottomBar.setOnItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.nav_guard -> inflateFragment(GuardFragment.newInstance())
+                R.id.nav_home -> inflateFragment(HomeFragment.newInstance())
+                R.id.nav_dashboard -> inflateFragment(DashBoardFragment.newInstance())
+                R.id.nav_profile -> inflateFragment(ProfileFragment.newInstance())
+            }
+            true
+        }
+    }
+
+    private fun inflateFragment(fragment : Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+}
